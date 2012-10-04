73068af20c404da398c7ba30f1cc050263fd1389
Zac Stewart <zgstewart@gmail.com>
2012-10-04 18:33:28 -0400

    Move model to gbm.r

--- gbm.r
+++ gbm.r
@@ -1,8 +1,9 @@
+#!/usr/bin/env Rscript
 library('gbm')
 
 print('Loading datasets...')
 census.data <- read.csv('data/training_filev1.csv')
-test.census.data <- read.csv('data/test_filev1.csv')
+test.census.data <- read.csv('data/test_filev1.clean.csv')
 
 print('Converting types...')
 for (i in 9:ncol(census.data)) {
@@ -20,12 +21,12 @@ gbm.census.formula = as.formula(paste('Mail_Return_Rate_CEN_2010 ~',
                                       paste(predictors, collapse=" + ")))
 
 print('Training GBM...')
-gbm.model <- gbm(gbm.census.formula, n.trees=5000, data=census.data,
+gbm.model <- gbm(gbm.census.formula, n.trees=7500, data=census.data,
              distribution='gaussian', interaction.depth=6,
              train.fraction=.8, cv.folds=5)
 
 print('Predicting...')
 gbm.best.iter <- gbm.perf(gbm.model, method='cv')
 predictions <- predict(gbm.model, newdata=test.census.data, gbm.best.iter)
-write.csv(predictions, 'submission.csv', row.names=FALSE)
-summary(model)
+write.csv(predictions, 'predictions/r-gbm.csv', row.names=FALSE)
+summary(gbm.model)
